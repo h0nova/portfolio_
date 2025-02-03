e7b4bffbad8efdc350b102ebf6b7ac863e3bea35
h0nova <timalysenko502@gmail.com>
2025-02-03 17:40:12 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(function(){
+  var mixer = mixitup('.projects__gallery')
+});
+
+$('.menu__button').click(function(){
+  $('.menu__button').toggleClass('menu__button--active');
+  $('.menu__dropdown').toggleClass('menu__dropdown--active');
+});
